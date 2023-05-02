26ded1196fed95dbdb57c543d8cdb41cacb50245
Jonathan Stines <jonathan.stines@gmail.com>
2023-05-02 14:15:16 -0500

    Create installnodeexporter.sh

---
+++ installnodeexporter.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+groupadd --system prometheus
+useradd -s /sbin/nologin --system -g prometheus prometheus
+mkdir /var/lib/prometheus
+for i in rules rules.d files_sd; do mkdir -p /etc/prometheus/${i}; done
+apt update
+apt-get -y install wget curl
+mkdir -p /tmp/prometheus && cd /tmp/prometheus
+curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest|grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
+tar xvf prometheus-2.43.0.linux-amd64.tar.gz
+cd prometheus-2.43.0.linux-amd64/
+mv prometheus promtool /usr/local/bin/
+mv prometheus.yml  /etc/prometheus/prometheus.yml
+mv consoles/ console_libraries/ /etc/prometheus/
+cd ~/
+rm -rf /tmp/prometheus
+cat /etc/prometheus/prometheus.yml
+tee /etc/systemd/system/prometheus.service<<EOF
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/usr/local/bin/prometheus   --config.file=/etc/prometheus/prometheus.yml   --storage.tsdb.path=/var/lib/prometheus   --web.console.templates=/etc/prometheus/consoles   --web.console.libraries=/etc/prometheus/console_libraries   --web.listen-address=0.0.0.0:9090   --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+tee /etc/systemd/system/prometheus.service<<EOF
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/usr/local/bin/prometheus   --config.file=/etc/prometheus/prometheus.yml   --storage.tsdb.path=/var/lib/prometheus   --web.console.templates=/etc/prometheus/consoles   --web.console.libraries=/etc/prometheus/console_libraries   --web.listen-address=0.0.0.0:9090   --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+for i in rules rules.d files_sd; do chown -R prometheus:prometheus /etc/prometheus/${i}; done
+for i in rules rules.d files_sd; do chmod -R 775 /etc/prometheus/${i}; done
+chown -R prometheus:prometheus /var/lib/prometheus/
+systemctl daemon-reload
+systemctl start prometheus
+systemctl enable prometheus
+systemctl status prometheus
+curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest| grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
+tar xzvf node_exporter-1.5.0.linux-amd64.tar.gz
+cd node_exporter-1.5.0.linux-amd64/
+cp node_exporter /usr/local/bin/
+tee /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+tee /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter
+systemctl status node_exporter
