b8ed7037b24fb5b8e5944470f7e6d6786839ae6c
Jonathan Stines <jonathan.stines@gmail.com>
2023-05-04 09:15:06 -0500

    Update and rename createpvelvm.sh to create_pve_vm_template.sh

--- create_pve_vm_template.sh
+++ create_pve_vm_template.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# script to create a lvm template using cloud-init for proxmox
+# script to create a pve vm template using cloud-init for proxmox
 
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img
 qm create 8200 --memory 8192 --name 2ubuntu-cloud --net0 virtio,bridge=vmbr0
-qm importdisk 8200 focal-server-cloudimg-amd64.img one_nvme
-qm set 8200 --ide2 one_nvme
-qm set 8200 --ide2 one_nvme:cloudinit
+qm importdisk 8200 focal-server-cloudimg-amd64.img local-lvm
+qm set 8200 --ide2 local-lvm:cloudinit
 qm set 8200 --boot c --bootdisk scsi0
 qm set 8200 --serial0 socket --vga serial0
